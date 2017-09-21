12bb578bacd4ae149f87c0fb8cec6e56112f0bf2
Ken Wagner <ken_xwagner@hotmail.com>
2017-09-20 23:41:05 -0700

    Added MVC
    Adding compiled MVC app.

---
+++ KenPortfolio/Identity_MVC/Identity_MVC/Identity_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Identity_MVC.Startup))]
+namespace Identity_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
