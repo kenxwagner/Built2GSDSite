47dc35f0288a5f15365619acadd6b4262f5ab02d
Ken Wagner <ken_xwagner@hotmail.com>
2017-09-20 23:53:25 -0700

    updates

--- KenPortfolio/Identity_MVC/Identity_MVC/Identity_MVC/Controllers/HomeController.cs
+++ KenPortfolio/Identity_MVC/Identity_MVC/Identity_MVC/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Identity_MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "You asked and we tell.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us.";
 
             return View();
         }
