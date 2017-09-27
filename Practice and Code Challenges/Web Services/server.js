17dd02250dcc7a269f2a85fd64e7951661d1d1d1
Ken Wagner <ken_xwagner@hotmail.com>
2017-09-27 01:46:29 -0700

    updates

--- Practice and Code Challenges/Web Services/server.js
+++ Practice and Code Challenges/Web Services/server.js
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <!-- make an api and fast please->
-  
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-
 var express = require('express');
 var app = express();
 var fs = require("fs");
@@ -31,6 +17,3 @@ var server = app.listen(8081, function () {
   console.log("Example app listening at http://%s:%s", host, port)
 
 })
-</body>
-<div class="bleh" nav-justified> Welcome back</>
-</html>
