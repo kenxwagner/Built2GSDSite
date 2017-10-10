e5d0888f32d363a83b7f6adf3fb88c2198b176bf
Ken Wagner <ken_xwagner@hotmail.com>
2017-10-10 06:30:20 -0700

    javascript bit

---
+++ Practice and Code Challenges/JavaScript/simpleClosure.js
@@ -0,0 +1,11 @@
+function displayName (firstName, lastName) {
+​var nameHello = "Hi!, your name must be ";
+    
+​function makeNameWhole () {
+​return nameHello + firstName + " " + lastName;    
+}
+​
+​return makeNameWhole ();
+}
+​
+displayNameName ("Oprah", "Winfrey");
