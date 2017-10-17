a121e879c2fcef5c91741f2ee6d415ff5ef3c0d8
Ken Wagner <ken_xwagner@hotmail.com>
2017-10-16 21:05:14 -0700

    added file

--- Practice and Code Challenges/JavaScript/simpleClosure2.js
+++ Practice and Code Challenges/JavaScript/simpleClosure2.js
@@ -1,1 +1,16 @@
-function something
+function somethingName (firstName){
+	var someIntro = "This celebrity is known as ";
+	//now this function has access to the outer function's variables, including the parameter
+function lastNameCall(theLastName){
+	return someIntro + firstName + " " + theLastName;
+}
+return lastNameCall;
+}
+	
+var mjName = somethingName("John");// At this juncture, the somethingName outer function has returned.​
+	​
+	​// The closure (lastNameCall) is called after the outer function has returned above​
+	​// Yet, the closure still has access to the outer function's variables and parameter​
+	mjName ("Wayne"); // This celebrity is John Wayne
+	
+	//Closures have access to the outer function’s variable even after the outer function returns
