d7557cb8259f8179329951a550922a6f95fa43c4
Ken Wagner <ken_xwagner@hotmail.com>
2017-12-08 16:33:28 -0800

    update

--- Code Library/MySQL/dbStartFile.js.js
+++ Code Library/MySQL/dbStartFile.js.js
@@ -1,2 +1,15 @@
 
 var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	
+	host: "localhost",
+	user: "yourusername",
+	password: "yourpassword"
+});
+
+con.connect(function(err){
+	if(err) throw err;
+	console.log("Connected!");
+	
+});
