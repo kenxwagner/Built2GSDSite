7d0089a07043c3fb0888f05d4eaec3a039442d77
Ken Wagner <ken_xwagner@hotmail.com>
2017-12-18 23:13:58 -0800

    added file

---
+++ Products/UseTheTalent/script/interviewChallenges.js
@@ -0,0 +1,18 @@
+//source: https://www.toptal.com/javascript/interview-questions
+var globalVar = "xyz";
+
+(function outerFunc(outerArg) {
+    var outerVar = 'a';
+    
+    (function innerFunc(innerArg) {
+    var innerVar = 'b';
+    
+    console.log(
+        "outerArg = " + outerArg + "\n" +
+        "innerArg = " + innerArg + "\n" +
+        "outerVar = " + outerVar + "\n" +
+        "innerVar = " + innerVar + "\n" +
+        "globalVar = " + globalVar);
+    
+    })(456);
+})(123);
