fc884ad4ee001da9858f55d42f62d4035975ebc2
Ken Wagner <ken_xwagner@hotmail.com>
2018-01-07 15:26:45 -0800

    Java fun

---
+++ Code Library/Java/stringThing1.java
@@ -0,0 +1,10 @@
+public class WelcomeHome
+{
+	static String randomString = "I have to say something!";
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println(randomString);
+	}
+}
