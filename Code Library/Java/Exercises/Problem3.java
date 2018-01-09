cb8a409f95d9dbb2fdcf74739fc0a3ec1e54f993
Ken Wagner <ken_xwagner@hotmail.com>
2018-01-09 13:55:16 -0800

    update

--- Code Library/Java/Exercises/Problem3.java
+++ Code Library/Java/Exercises/Problem3.java
@@ -5,8 +5,12 @@ public class SayHello
 	
 	public static void main(String[] args)
 	{
+		
 		System.out.println(32+25);
 		System.out.println(32/4);
-		System.out.println(5*25);
+		System.out.println(-5 + 8 * 6);
+		System.out.println((55+9) % 9 );
+		System.out.println( 20 + -3*5 / 8);
+		System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
 	}
 }
