f79bbabfe9fc988db4a1a08cf72abb61e5ccd364
Ken Wagner <ken_xwagner@hotmail.com>
2018-01-25 09:27:39 -0800

    hashtable

---
+++ Code Library/Java/hashTable1.java
@@ -0,0 +1,32 @@
+import java.util.Hashtable;
+import java.util.Enumeration;
+
+public class HashtableTimes
+{
+	
+	
+	
+	public static void main(String[] args)
+	{
+		Enumeration names;
+		String key;
+ 
+   // Creating a Hashtable
+   Hashtable<String, String> hashtable = 
+              new Hashtable<String, String>();
+ 
+   // Adding Key and Value pairs to Hashtable
+   hashtable.put("Key1","Banana");
+   hashtable.put("Key2","Dragon Fruit");
+   hashtable.put("Key3","Kiwi");
+   hashtable.put("Key4","Pineapple");
+   hashtable.put("Key5","Strawberry");
+ 
+   names = hashtable.keys();
+   while(names.hasMoreElements()) {
+      key = (String) names.nextElement();
+      System.out.println("Key: " +key+ " & Value: " +
+      hashtable.get(key));
+   }
+	}
+}
