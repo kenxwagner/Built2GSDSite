05e35ec27319055b21f0a61e2a99353830b3f3f2
Ken Wagner <ken_xwagner@hotmail.com>
2018-02-09 15:36:42 -0800

    Updates to bst

--- Code Library/Java/binarySearchTree2.java
+++ Code Library/Java/binarySearchTree2.java
@@ -19,10 +19,27 @@ public class BinaryTree {
 			
 			while (true){
 				
+				
 				parent = focusNode;
 				
 				if (key < focusNode.key) {
 					focusNode = focusNode.leftChild;
+					
+					if (focusNode == null) {
+						
+						parent.leftChild = newNode;
+						return;
+					}
+					
+				} else { 
+					
+					focusNode = focusNode.rightChild;
+					
+					if (focusNode == null) {
+						
+						parent.rightChild = newNode;
+						return;
+					}
 				}
 			}
 			
@@ -30,4 +47,16 @@ public class BinaryTree {
 		}
 		
 	}
-}
+	public static void main(String[] args) {
+		
+		public void inOrderTraverseTree(Node focusNode) {
+       if (focusNode != null) {
+            // Traverse the left node
+            inOrderTraverseTree(focusNode.leftChild);
+            // Visit the currently focused on node
+            System.out.println(focusNode);
+            // Traverse the right node
+            inOrderTraverseTree(focusNode.rightChild);
+        }		
+	}
+	}
