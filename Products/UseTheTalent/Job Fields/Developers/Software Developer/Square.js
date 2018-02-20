18601d90c8c4fb437f59fceb8fc30a95bbc3a13b
Ken Wagner <ken_xwagner@hotmail.com>
2018-02-19 21:20:36 -0800

    update

--- Products/UseTheTalent/Job Fields/Developers/Software Developer/Square.js
+++ Products/UseTheTalent/Job Fields/Developers/Software Developer/Square.js
@@ -19,8 +19,23 @@ class Board estends React.Component {
 		const status = 'Next player: X';
 		
 		return (
-			<div>
-			<div className="status">{status}</div>
+			<div><div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
 		)
 	}
 	
