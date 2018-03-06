3f45f37625ec2232b21102a65f557922e0761034
Ken Wagner <ken_xwagner@hotmail.com>
2018-03-05 16:35:08 -0800

    update

--- Products/UseTheTalent/Job Fields/Developers/Web Developer/Ajax/showCustomer.js
+++ Products/UseTheTalent/Job Fields/Developers/Web Developer/Ajax/showCustomer.js
@@ -1,6 +1,15 @@
-function showCustomer(str){
-	var xhttp; 
+function showCustomer(str) {
+  var xhttp; 
   if (str == "") {
     document.getElementById("txtHint").innerHTML = "";
     return;
+  }
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("txtHint").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "getcustomer.asp?q="+str, true);
+  xhttp.send();
 }
