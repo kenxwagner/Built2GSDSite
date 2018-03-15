1411952afa82d8694650b59df707ff45e3a8d721
Ken Wagner <ken_xwagner@hotmail.com>
2018-03-14 21:29:39 -0700

    Database
    Created and table built

---
+++ Products/Scheduler/SQL/schedule1DB.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE schedule1DB;
+
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255) 
+);
+
+CREATE TABLE schedule_calendar (
+	DayofWeek varchar(10)
+
+);
