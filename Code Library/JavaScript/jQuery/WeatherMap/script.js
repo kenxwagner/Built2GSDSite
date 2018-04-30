4d9e9d399aaad468c1f0af18091b45b189f6027b
Ken Wagner <ken_xwagner@hotmail.com>
2018-04-30 14:51:59 -0700

    Practice with API

---
+++ Code Library/JavaScript/jQuery/WeatherMap/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  //Get Location
+  navigator.geolocation.getCurrentPosition(success, error);
+  
+  function success(pos) {
+    var lat = pos.coords.latitude;
+    var long = pos.coords.longitude;
+    weather(lat, long);
+  }
+  
+  function error() {
+    console.log('There was an error');
+  }
+  function weather(lat, long) {
+     var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
+    
+    $.getJSON(URL, function(data){
+          updateDOM(data);
+    });
+  }
+  //weather();
+  
+  function updateDOM(data) {
+    var city = data.name;
+    var temp = Math.round(data.main.temp);
+    var desc = data.weather[0].description;
+    var icon = data.weather[0].icon;
+    
+    $('#city').html(city);
+    $('#temp').html(temp);
+    $('#desc').html(desc);
+    $('#icon').attr('src',icon);
+  }
+});
