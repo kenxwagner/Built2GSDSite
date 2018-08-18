f3ed78371fbb2a9f90d9db9362858730d8cd3f53
Ken Wagner <ken_xwagner@hotmail.com>
2018-08-17 23:36:21 -0700

    More

--- Products/methodForTalent/method-for-talent/src/App.js
+++ Products/methodForTalent/method-for-talent/src/App.js
@@ -14,6 +14,9 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+	  <div className="App-body">
+	  <p>This should be changed out.</p>
+	  </div>
     );
   }
 }
