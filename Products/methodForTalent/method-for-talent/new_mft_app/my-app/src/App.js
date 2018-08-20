daac3317674b01018f2c0503d73f547ab2679fba
Ken Wagner <ken_xwagner@hotmail.com>
2018-08-19 23:29:43 -0700

    Test coming

--- Products/methodForTalent/method-for-talent/new_mft_app/my-app/src/App.js
+++ Products/methodForTalent/method-for-talent/new_mft_app/my-app/src/App.js
@@ -17,6 +17,20 @@ class App extends Component {
 	 
     );
   }
+  render() {
+    return (
+      <div className="Fake1">
+        <header className="Fake1App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="FakeApp-intro">
+          Study and learn more. This is a quick lesson. We are editing <code>src/App.js</code> and saving to reload.
+        </p>
+      </div>
+	 
+    );
+  }
 }
 
 export default App;
