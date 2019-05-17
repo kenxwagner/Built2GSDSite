ecc4029e5d6d5032a28206a77605b3a700b86699
Ken Wagner <ken_xwagner@hotmail.com>
2019-05-16 22:39:32 -0700

    Update

--- ReactVersion/built2gsd/src/App.js
+++ ReactVersion/built2gsd/src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>Just to get started, here is a Title: Built2GSD</h1>
+		<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
